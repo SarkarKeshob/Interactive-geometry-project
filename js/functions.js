a1265438804ba6538aa40e786d441bd77122858d
Shop Queen Dot Com Dot Bd <keshob.sarkar.shuvo@gmail.com>
2023-12-19 03:07:27 +0600

    uploading updated version off the project

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,7 @@ function valueChecker(string1,string2){
     let num1= idElement(string1).value;
     let num2 = idElement(string2).value;
     if((isNaN(num1))|| (isNaN(num2))||(num1==='')||(num2==='')){
-        areaShower('Please Insert A Valid Number !!!!');
+       alert('Please Insert A Valid Number !!!!');
         return 0;
     }
 
@@ -18,10 +18,17 @@ function valueChecker(string1,string2){
 
 }
 
-function areaShower(string1){
-    let area= idElement('area-calculator');
-    area.innerText= string1;
+function areaShower(string1,string2,string3){
+   let area=idElement(string2);
+    area.innerText=string1;
+}
 
-
+function areaCalculator(area,areaType){
+    let areaCalculator= idElement('area-calculator');
+    let count=areaCalculator.childElementCount;
+    let p= document.createElement('p');
+    p.classList.add('mb-4')
+    p.innerHTML=`${count+1}. ${areaType} ${area} cm<sup>2</sup> <button class='btn btn-sm btn-accent'>Convert</button>`
+    areaCalculator.appendChild(p);
 }
 
--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ triangleCalculate.addEventListener('click',function(){
     let check= valueChecker('triangle-base','triangle-height');
     if(check!==0){
         
-        areaShower('The Area of Your Triangle Is =  '+(0.5*check)+' square cm ');
+        areaShower(('The Area of Your Triangle Is =  '+(0.5*check)+' square cm '),('triangle-area'));
+
+        areaCalculator((0.5*check),'Triangle');
         idElement('triangle-base').value='';
         idElement('triangle-height').value='';
     }
@@ -21,9 +23,8 @@ let rectangleCalculate=idElement('rectangle-calculate');
 rectangleCalculate.addEventListener('click',function(){
     let check= valueChecker('rectangle-width','rectangle-length');
     if(check!==0){
-        areaShower('The Area of Your Rectangle Is =  '+check+' square cm ');
-        rectangleLength.value='';
-        rectangleWidth.value='';
+        areaShower(('The Area of Your Rectangle Is =  '+check+' square cm '),('rectangle-area'));
+        areaCalculator((check),'Rectangle');
         idElement('rectangle-width').value='';
         idElement('rectangle-length').value='';
     
@@ -42,7 +43,8 @@ let parallelogramCalculate=idElement('parallelogram-calculate');
 parallelogramCalculate.addEventListener('click',function(){
     let check= valueChecker('parallelogram-base','parallelogram-height');
     if(check!==0){
-        areaShower('The Area of Your Parallelogram Is =  '+check+' square cm ');
+        areaShower(('The Area of Your Parallelogram Is =  '+check+' square cm '),('parallelogram-area'));
+        areaCalculator((check),'Parrallelogram');
         idElement('parallelogram-base').value='';
         idElement('parallelogram-height').value='';
     }
@@ -58,7 +60,8 @@ let rhombusCalculate=idElement('rhombus-calculate');
 rhombusCalculate.addEventListener('click',function(){
     let check= valueChecker('rhombus-dia1','rhombus-dia2');
     if(check!==0){
-        areaShower('The Area of Your Rhombus Is =  '+(0.5*check)+' square cm ');
+        areaShower(('The Area of Your Rhombus Is =  '+(0.5*check)+' square cm '),('rhombus-area'));
+        areaCalculator((0.5*check),'Rhombus');
         idElement('rhombus-dia1').value='';
         idElement('rhombus-dia2').value='';
     }
@@ -75,7 +78,8 @@ let pentagonCalculate=idElement('pentagon-calculate');
 pentagonCalculate.addEventListener('click',function(){
     let check= valueChecker('pentagon-p','pentagon-b');
     if(check!==0){
-        areaShower('The Area of Your Pentagon Is =  '+(0.5*check)+' square cm ');
+        areaShower(('The Area of Your Pentagon Is =  '+(0.5*check)+' square cm '),('pentagon-area'));
+        areaCalculator((0.5*check),'Pentagon');
         idElement('pentagon-p').value='';
         idElement('pentagon-b').value='';
     }
@@ -91,7 +95,8 @@ let ellipseCalculate=idElement("ellipse-calculate");
 ellipseCalculate.addEventListener('click',function(){
     let check= valueChecker("ellipse-breadth","ellipse-height");
     if(check!==0){
-        areaShower('The Area of Your Ellipse Is =  '+(3.1416*check)+' square cm ');
+        areaShower(('The Area of Your Ellipse Is =  '+(3.1416*check)+' square cm '),('ellipse-area'));
+        areaCalculator((3.1416*check),'Ellipse');
         idElement("ellipse-breadth").value='';
         idElement("ellipse-height").value='';
     }
